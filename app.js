6c1c9e5630ff9deb948126790a8cbe4fce04597c
trinhthang2212 <trinhthang1122@gmail.com>
2017-04-16 19:39:05 +0700

    delete comment

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ console.log("Hello nodemon");
 
 const fs = require('fs');
 
-// fs.writeFileSync('firstapp.txt','Hello world, file system');
-//
-// fs.text = 'a';
-// var content = fs.readFileSync('firstapp.txt', 'utf-8');
-//
-// console.log(content);
-
 //su dung thu vien express
 const express = require('express');
 var app = express();
@@ -19,10 +12,6 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.send('./public/index.html');
 });
-
-// app.get('/image/hotgirl', (req, res) => {
-//   res.send('<img src="http://cms.kienthuc.net.vn/zoom/1000/uploaded/nguyenanhson/2016_04_29/4/hot-girl-tra-sua-dep-nhu-nang-mai-tai-da-lat-hinh-5.jpg" style="width:840px;height:640px;">');
-// })
 
 app.get('/image/add', (req, res) =>{
   var imageinfo = {
@@ -54,4 +43,3 @@ app.get('/image/get', (req, res) =>{
 app.listen(6969, (req, res) => {
   console.log('hey trinhthang.....app listen on 6969');
 });
-// arrow function chuyen function thanh =>
